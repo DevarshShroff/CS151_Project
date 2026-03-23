3aa9ee2249ac2eae98dc802a7bfcdc6e1e3250d8
Shantall Cabrejo <149267844+ShantallCabrejo@users.noreply.github.com>
2026-03-23 14:10:18 -0700

    Refactor winner determination method in ClassicRules

--- Rock-Paper-Scissors/src/main/ClassicRules.java
+++ Rock-Paper-Scissors/src/main/ClassicRules.java
@@ -1,22 +1,20 @@
 public class ClassicRules implements IRules {
-    // Formerly GameLogic, now ClassicRules class
-    // Now implements IRules interface for game logic
 
     @Override
-    public int determineWinner(Move p1, Move p2) {
-        // For testing purposes: 
-        // We know Paper (2) vs Scissors (3) results in a Computer Win (2)
-        if(p1 == p2){
-            return 0;
+    public int decideWinner(Move p1, Move p2) {
+        if (p1 == p2) {
+            return 0; // tie
         }
 
-        if((p1 == Move.ROCK && p2 == Move.SCISSORS) ||
-           (p1 == Move.PAPER && p2 == Move.ROCK) ||
-           (p1 == Move.SCISSORS && p2 == Move.PAPER)){
-            return 1;
-           }
-
-        return 2;
+        switch (p1) {
+            case ROCK:
+                return (p2 == Move.SCISSORS) ? 1 : 2;
+            case PAPER:
+                return (p2 == Move.ROCK) ? 1 : 2;
+            case SCISSORS:
+                return (p2 == Move.PAPER) ? 1 : 2;
+            default:
+                return 0;
+        }
     }
-
 }
