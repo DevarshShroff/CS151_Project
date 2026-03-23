5660abf12cfaee2c4c6b9d9406487b19f5db6964
Shantall Cabrejo <149267844+ShantallCabrejo@users.noreply.github.com>
2026-03-23 14:14:49 -0700

    Refactor ComputerPlayerFactory to use ComputerPlayer

--- Rock-Paper-Scissors/src/main/ComputerPlayerFactory.java
+++ Rock-Paper-Scissors/src/main/ComputerPlayerFactory.java
@@ -1,16 +1,11 @@
 public class ComputerPlayerFactory {
-    
     public static Player createComputerPlayer(Algorithm algo) {
         switch (algo) {
             case MACHINE_LEARNING:
-                return new MLComputerPlayer();
-                //Implement MLComputerPlayer (MLComputerPlayer.java)
+                return new ComputerPlayer("CPU (Machine Learning)", new MLComputerPlayer());
             case RANDOM:
             default:
-                return new RandomComputerPlayer();
-                //Implement RandomComputerPlayer (RandomComputerPlayer.java)
-
+                return new ComputerPlayer("Computer (Random)", new RandomComputerPlayer());
         }
     }
 }
-
